25f6fe7d3cf5cb6550194932298f62ede025df8d
Rlss91 <rlss91@gmail.com>
2022-06-27 10:18:30 +0300

    amazon fake 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import "./App.css";
 import Navbar from "./components/navbar/navbar.component";
-import TestComponent from "./components/Test.component";
-import LoginPage from "./pages/login/Login.page";
-import ProductsPage from "./pages/products/Products.page";
-import RegisterPage from "./pages/register/Register.page";
-import UsersPage from "./pages/users/Users.page";
+// import TestComponent from "./components/Test.component";
+// import LoginPage from "./pages/login/Login.page";
+// import ProductsPage from "./pages/products/Products.page";
+// import RegisterPage from "./pages/register/Register.page";
+// import UsersPage from "./pages/users/Users.page";
+import DefenetlyRealAmazonPage from "./pages/defenetlyRealAmazon/defenetlyRealAmazon.page";
 
 function App() {
   return (
     <div className="container">
       <Navbar />
-      <TestComponent />
+      {/* <TestComponent /> */}
       {/* <LoginPage /> */}
       {/* <RegisterPage /> */}
       {/* <ProductsPage /> */}
-      <UsersPage />
+      {/* <UsersPage /> */}
+      <DefenetlyRealAmazonPage />
     </div>
   );
 }
---
+++ src/pages/defenetlyRealAmazon/defenetlyRealAmazon.page.jsx
@@ -0,0 +1,11 @@
+import AmazonCardContainerComponent from "../../components/amazonCardContainer/amazonCardContainer.component";
+
+const DefenetlyRealAmazonPage = () => {
+  return (
+    <div>
+      <AmazonCardContainerComponent />
+    </div>
+  );
+};
+
+export default DefenetlyRealAmazonPage;
