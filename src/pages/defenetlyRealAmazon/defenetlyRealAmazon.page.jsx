c7385d6e6926dd103d187dd1ec4722a665df2831
Rlss91 <rlss91@gmail.com>
2022-06-27 11:35:19 +0300

    fake amazon

--- src/pages/defenetlyRealAmazon/defenetlyRealAmazon.page.jsx
+++ src/pages/defenetlyRealAmazon/defenetlyRealAmazon.page.jsx
@@ -1,9 +1,11 @@
 import AmazonCardContainerComponent from "../../components/amazonCardContainer/amazonCardContainer.component";
+import AmazonItemsContainerComponent from "../../components/amazonItemsContainer/amazonItemsContainer.component";
 
 const DefenetlyRealAmazonPage = () => {
   return (
     <div>
       <AmazonCardContainerComponent />
+      <AmazonItemsContainerComponent />
     </div>
   );
 };
