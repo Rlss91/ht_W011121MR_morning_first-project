9a6e6756d2c0931cbab5bd9dbc130d437742e80f
Rlss91 <rlss91@gmail.com>
2022-06-27 14:03:29 +0300

    axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import Navbar from "./components/navbar/navbar.component";
+import FirstAjaxPage from "./pages/firstAjax/firstAjaxPage";
 // import TestComponent from "./components/Test.component";
 // import LoginPage from "./pages/login/Login.page";
 // import ProductsPage from "./pages/products/Products.page";
 // import RegisterPage from "./pages/register/Register.page";
 // import UsersPage from "./pages/users/Users.page";
-import DefenetlyRealAmazonPage from "./pages/defenetlyRealAmazon/defenetlyRealAmazon.page";
+// import DefenetlyRealAmazonPage from "./pages/defenetlyRealAmazon/defenetlyRealAmazon.page";
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
       {/* <RegisterPage /> */}
       {/* <ProductsPage /> */}
       {/* <UsersPage /> */}
-      <DefenetlyRealAmazonPage />
+      {/* <DefenetlyRealAmazonPage /> */}
+      <FirstAjaxPage />
     </div>
   );
 }
---
+++ src/pages/firstAjax/firstAjaxPage.jsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const FirstAjaxPage = () => {
+  const handleBtnClick = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((data) => {
+        console.log("data from axios", data.data);
+      })
+      .catch((err) => console.log("errr", err));
+  };
+  return (
+    <button className="btn btn-success" onClick={handleBtnClick}>
+      send request
+    </button>
+  );
+};
+
+export default FirstAjaxPage;
